87b23ea7bc2bcf39f2ad180c65892b561cd129ce
elLuiz <elLuiz@gitlab>
2024-05-14 16:50:00 -0300

    Unique strophes

--- service/lyricsGroupService.js
+++ service/lyricsGroupService.js
@@ -1,10 +1,9 @@
 function createStropheGroups(lyrics) {
     if (isNotEmpty(lyrics)) {
-        var groups = lyrics.split(/\n{2}/gm)
+        var groups = new Set(lyrics.split(/\n{2}/gm).map(strophe => strophe.trim()))
         const groupsMap = new Map()
         let groupId = 1
         groups.forEach(strophe => {
-            strophe = strophe.trim()
             if (!strophe.match(/\d+/) && strophe.trim().length > 0) {
                 groupsMap.set(groupId, strophe)
                 groupId++
